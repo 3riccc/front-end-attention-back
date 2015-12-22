917dcd49a169abc845929bc2924a527d5f6ff6bd
3riccc <ericinisa@163.com>
2015-12-22 11:46:29 +0800

    查询结果

--- searchPaper.php
+++ searchPaper.php
@@ -1,2 +1,9 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
+	$key = $_GET['key'];
+	// 这里是一些查询逻辑
 
+
+
+	$listArr = array(array('id'=>0,'title'=>'查询结果1','author'=>'张三'),array('title'=>'查询结果二','author'=>'李四','id'=>1),array('title'=>'结果三','id'=>-1,'author'=>'作者的名字很长很长很长很长城'));
+	echo json_encode(listArr);
